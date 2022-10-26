0300c5e1f3eb263c247aba0202e30f8319488096
Kevork <kevorkchristian@gmail.com>
2022-10-26 17:22:04 +0200

    scapy receiver but icmp is tricky and malformed

--- task1_sender.py
+++ task1_sender.py
@@ -1,28 +1,41 @@
-import socket
+from scapy.all import ICMP, IP, sr, raw
 import sys
 
-argc = len(sys.argv)
+def generate_icmp_packet(data: str, source_ip: str, dest_ip: str, dest_port: int=21):
+    # return IP(raw(IP(dst=sys.argv[1], src=sys.argv[1])/ICMP(type=0) / data.encode("utf-8")))
+    return IP(dst=sys.argv[1], src=sys.argv[1])/ICMP(type=0) / data.encode("utf-8")
 
-if argc != 3:
-    print("Usage: python3 task1_sender.py <IP address> <port number>")
-    sys.exit(1)
+packet = generate_icmp_packet("Hello World", source_ip="127.0.0.1", dest_ip=sys.argv[1], dest_port=sys.argv[2])
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
+print(packet.show())
 
-# Construct ICMP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) # AF_INET = IPv4, SOCK_RAW = raw socket, IPPROTO_ICMP = ICMP
+sr(packet, retry=5, timeout=1.5, iface="lo0")
 
-s.connect((ip, port)) # Bind to port 0, which means any available port
+# import socket
+# import sys
 
-while True:
-    message = input("Enter message to send the reicever: ")
+# argc = len(sys.argv)
 
-    # Encrypt with AES-GCM
-    # TODO
+# if argc != 3:
+#     print("Usage: python3 task1_sender.py <IP address> <port number>")
+#     sys.exit(1)
 
-    # Send the message
+# ip = sys.argv[1]
+# port = int(sys.argv[2])
+
+# # Construct ICMP socket
+# s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) # AF_INET = IPv4, SOCK_RAW = raw socket, IPPROTO_ICMP = ICMP
+
+# s.connect((ip, port)) # Bind to port 0, which means any available port
+
+# while True:
+#     message = input("Enter message to send the reicever: ")
+
+#     # Encrypt with AES-GCM
+#     # TODO
+
+#     # Send the message
     
-    print(f"Sending the following message to {ip}:{port}\n{message}")
+#     print(f"Sending the following message to {ip}:{port}\n{message}")
 
-    s.send(message.encode())
+#     s.send(message.encode())
