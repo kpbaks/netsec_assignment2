51db53eeb57058e17ccf1ab6d3860348dabdeb50
Kevork <kevorkchristian@gmail.com>
2022-10-27 11:34:04 +0200

    loop bug fixed

--- icmp.py
+++ icmp.py
@@ -4,4 +4,4 @@ import struct
 icmp_header_format = '!BBHI' # ! = network byte order, B = unsigned char, H = unsigned short, I = unsigned int
 icmp_type = 47 # ICMP covert espionage request
 icmp_code = 0 # Irrelevant for covert espionage request
-icmp_checksum = 0 # Pseudo checksum to be calculated later
+icmp_temp_checksum = 0 # Pseudo checksum to be calculated later
--- task1_sender2.py
+++ task1_sender2.py
@@ -21,15 +21,15 @@ def checksum(data: bytes) -> int:
     return checksum
 
 def send_covert_message(ip: str = "127.0.0.1", port: int=9000, message: str="Hello World"):
-    icmp_header: bytes = struct.pack(icmp_header_format, icmp_type, icmp_code, icmp_checksum, 0) # Rest of header field is irrelevant for our message
+    icmp_header: bytes = struct.pack(icmp_header_format, icmp_type, icmp_code, icmp_temp_checksum, 0) # Rest of header field is irrelevant for our message
 
     # The payload we want to send the receiver
-    icmp_payload = b'Hello World'
+    icmp_payload = bytes(message, encoding="utf-8")
 
     # Set the checksum
-    icmp_header = struct.pack(icmp_header_format, icmp_type, icmp_code, icmp_checksum, 0)
+    icmp_header = struct.pack(icmp_header_format, icmp_type, icmp_code, icmp_temp_checksum, 0)
 
-    print(f"Type: {icmp_type}, Code: {icmp_code}, Checksum: {icmp_checksum}, Data: {icmp_payload}")
+    #print(f"Type: {icmp_type}, Code: {icmp_code}, Checksum: {icmp_temp_checksum}, Data: {icmp_payload}")
 
     # Encrypting the payload and sending the nonce and tag witht he packet
     nonce = secrets.token_bytes(16)
@@ -43,12 +43,13 @@ def send_covert_message(ip: str = "127.0.0.1", port: int=9000, message: str="Hel
     # Send the packet
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     packet = icmp_header + nonce + tag + ciphertext
-    print(f"len(header): {len(icmp_header)}, len(nonce): {len(nonce)}, len(tag): {len(tag)}, len(ciphertext): {len(ciphertext)}")
+    #print(f"len(header): {len(icmp_header)}, len(nonce): {len(nonce)}, len(tag): {len(tag)}, len(ciphertext): {len(ciphertext)}")
     s.sendto(packet, (sys.argv[1], int(sys.argv[2])))
+    print("Message sent successfully")
 
 while True:
     message = input("Enter message to covertly send: ")
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 3: # If the user has specified an IP and port
         send_covert_message(sys.argv[1], int(sys.argv[2]), message)
     else:
         send_covert_message(message=message)
